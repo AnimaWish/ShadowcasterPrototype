8bfad047801bdc6f37f8da11ae35177a163e67db
Caden <xbartonc@gmail.com>
2016-09-20 21:20:38 -0600

    Added scroll wheel y-position movement to ISO view. Fixed collision issue.

--- Shadowcast Prototype Project/Assets/Scripts/GoToMouse.cs
+++ Shadowcast Prototype Project/Assets/Scripts/GoToMouse.cs
@@ -6,8 +6,9 @@ public class GoToMouse : MonoBehaviour
 
     public float speed = 1.5f;
     private Vector3 target;
-    LayerMask groundMask; 
-    float moveSpeed = 0.1f;
+    LayerMask groundMask;
+    //float moveSpeed = 0.1f;
+    float moveSpeed = 8f;
     bool drag;
 
     void Start()
@@ -24,11 +25,13 @@ public class GoToMouse : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             drag = true;
+            gameObject.GetComponent<Rigidbody>().useGravity = false; // Added for scroll wheel up and down so cube can float
         }
 
         else if (Input.GetMouseButtonUp(0))
         {
             drag = false;
+            gameObject.GetComponent<Rigidbody>().useGravity = true; // Added for scroll wheel up and down so cube can float
         }
 
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundMask))
@@ -42,15 +45,27 @@ public class GoToMouse : MonoBehaviour
     void Move(Vector3 TargetPos)
     {
         TargetPos.y = transform.position.y;
+
+        // Scroll Wheel code:
+        var yPos = Input.GetAxis("Mouse ScrollWheel");
+        if (yPos > 0f) // Scroll Up
+        {
+            TargetPos.y+=2;
+        }
+        else if (yPos < 0f) // Scroll Down
+        {
+            TargetPos.y-=2;
+        }
+        
         Vector3 StartPosition = transform.position;
         Vector3 EndPosition = TargetPos;
 
-        float t = 0.0f;
+        //float t = 0.0f;
 
-        while (t < 1.0)
-        {
-            t += Time.deltaTime * moveSpeed;
-            transform.position = Vector3.Lerp(StartPosition, EndPosition, t);
-        }
+        //while (t < 1.0)
+        //{
+            //t += Time.deltaTime * moveSpeed;
+            transform.position = Vector3.Lerp(StartPosition, EndPosition, Time.deltaTime * moveSpeed);
+        //}
     }
 }
